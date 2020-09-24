418e5df734ba9d282600c4c1acd1e3b036de1a43
Ave-Wat <wattsa2@carleton.edu>
2020-09-23 21:31:47 -0500

    update book.py for full line

--- books/book.py
+++ books/book.py
@@ -5,6 +5,7 @@ class Book:
         authorList = author.split("(")
         self.authorName = authorList[0].rstrip()
         self.authorYears = authorList[1].rstrip()
+        self.fullLine = title + pubYear + author
         
     def getTitle(self):
         return self.title
@@ -17,6 +18,9 @@ class Book:
     
     def getAuthorYears(self):
         return self.authorYears
+    
+    def getFullLine(self):
+        return self.fullLine
     
     def printBook(self):
         print(self.title, end=',')
