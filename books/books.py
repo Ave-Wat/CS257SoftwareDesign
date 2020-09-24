b1a0a98b5cc254f785adf6ff307755a22e2e00ac
Ave-Wat <wattsa2@carleton.edu>
2020-09-23 21:45:21 -0500

    search title, year

--- books/books.py
+++ books/books.py
@@ -11,6 +11,43 @@ with open("books.csv", 'r') as csvfile:
     for row in csvreader:
         library.append(Book(row[0], row[1], row[2]))
 print(library)
+    
+def searchTitle(searchString):
+    searchedBooks = []
+    for book in library:
+        if searchString in book.getTitle():
+            searchedBooks.append(book)
+    return searchedBooks
 
-#determineCommands(sys.argv)
+def searchYears(searchKey):
+    searchedBooks = []
+    for book in library:
+        if book.getPubYear() == searchKey:
+            searchedBooks.append(book)
+    return searchedBooks
+
+def searchAll(searchString):
+    searchedBooks = []
+    for book in library:
+        if searchString in book.getFullLine():
+            searchedBooks.append(book)
+    return searchedBooks
+    
+def printBooks(bookList):
+    for book in bookList:
+        book.printBook()
+        
+
+def determineCommands():
+    #program name is sys.argv[0]
+    if sys.argv[1] == "print":
+        option = sys.arv[2]
+        if option == "--title":
+            searchBooks()
+    else if sys.arv[1] == "help":
+        #print usage.txt file
+    else:
+        #print error message
+    
+    
     
