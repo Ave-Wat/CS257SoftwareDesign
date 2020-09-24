d1de5700f5617b9752a04bffeace4ce73d9d01b7
Rebecca Hicke <rmhicke@eduroam-163-72.dyn.carleton.edu>
2020-09-24 11:12:43 -0500

    Added help() function

--- books/books.py
+++ books/books.py
@@ -10,44 +10,6 @@ def readFile():
         for row in csvreader:
             library.append(Book(row[0], row[1], row[2]))
 
-def compare(itemOne, itemTwo, sortBy):
-    if sortBy == "author":
-        itemOne = itemOne.getAuthorName().split(" ")[1].lower()
-        itemTwo = itemTwo.getAuthorName().split(" ")[1].lower()
-    elif sortBy == "year":
-        itemOne = int(itemOne.getPubYear())
-        itemTwo = int(itemTwo.getPubYear())
-    elif sortBy == "title":
-        itemOne = itemOne.getTitle().lower()
-        itemTwo = itemTwo.getTitle().lower()
-        
-    if itemOne < itemTwo:
-        return True
-    return False
-
-def sortBooks(searchedBooks, sortBy):
-    if len(searchedBooks) > 1:
-        leftSide = sortBooks(searchedBooks[:len(searchedBooks)//2], sortBy)
-        rightSide = sortBooks(searchedBooks[len(searchedBooks)//2:], sortBy)
-    
-        sortedBooks = []
-        while (len(leftSide) > 0 and len(rightSide) > 0):
-            if (compare(leftSide[0], rightSide[0], sortBy)):
-                sortedBooks.append(leftSide[0])
-                leftSide.pop(0)
-            else:
-                sortedBooks.append(rightSide[0])
-                rightSide.pop(0)
-    
-        if len(leftSide) != 0:
-            sortedBooks += leftSide
-        elif len(rightSide) != 0:
-            sortedBooks += rightSide
-    else:
-        sortedBooks = searchedBooks
-        
-    return sortedBooks
-
 def searchAuthors(searchString):
     searchedBooks = []
     for book in library:
@@ -68,13 +30,13 @@ def searchYears(searchString):
     listYears = searchString.split("-")
     searchedBooks = []
     
-    startYear = listYears[0]
-    endYear = listYears[1]
+    startYear = int(listYears[0])
+    endYear = int(listYears[1])
     
     for book in library:
-        for i in range(startYear,endYear + 1)
-            if book.getPubYear() == i:
-            searchedBooks.append(book)
+        for i in range(startYear,endYear + 1):
+            if int(book.getPubYear()) == i:
+                searchedBooks.append(book)
     return sorted(searchedBooks, key = lambda Book: int(Book.pubYear))
 
 def searchAll(searchString):
@@ -83,6 +45,13 @@ def searchAll(searchString):
         if searchString in book.getFullLine():
             searchedBooks.append(book)
     return sorted(searchedBooks, key = lambda Book: Book.authorName.split(" ")[1].lower())
+
+def help():
+    with open('usage.txt') as usage:
+        usage = usage.readlines()
+        for line in usage:
+            print(line, end="")
+    print()
     
 def printBooks(bookList):
     for book in bookList:
@@ -95,8 +64,9 @@ def determineCommands():
     length = len(sys.argv)
     
     if length < 2:
+        pass
         #print error and suggest help
-    else if sys.argv[1] == "print":
+    elif sys.argv[1] == "print":
         option = sys.argv[2]
         
         if length < 3:
@@ -105,15 +75,18 @@ def determineCommands():
             if length == 3:
                 if option == "--title":
                     searchTitle(sys.argv[3])
-                else if option == "--years":
+                elif option == "--years":
                     searchYears(sys.argv[3])
-                else if option == "--author":
+                elif option == "--author":
                     searchAuthors(sys.argv[3])
             else:
+                pass
                 #print error: too many cmnd line entries
-    else if sys.arv[1] == "help":
+    elif sys.arv[1] == "help":
+        pass
         #print usage.txt file
     else:
+        pass
         #print error message and suggest help
 
     if sys.argv[1] == "print":
@@ -127,14 +100,9 @@ def determineCommands():
         pass
         #print error message
     
-#TODO add help method
 
-#def main():
-    #readFile()
+def main():
+    readFile()
     #determineCommands()
 
-readFile()
-sorted_books = searchTitle("an")
-for book in sorted_books:
-    book.printBook()
-
+main()
