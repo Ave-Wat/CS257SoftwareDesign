b5ccd466c7fefbfd793c26dacccd3de4180e7264
Ave-Wat <wattsa2@carleton.edu>
2020-09-24 12:02:07 -0500

    book.py file comment update

--- books/book.py
+++ books/book.py
@@ -1,10 +1,13 @@
 class Book:
+    #this class represents books from the .csv file. Each Book has a title, a year of publication, an author name, and years denoting the author's life.
     def __init__(self, title, pubYear, author):
         self.title = title
         self.pubYear = pubYear
         authorList = author.split("(")
         self.authorName = authorList[0].rstrip()
         self.authorYears = authorList[1].rstrip()
+        
+        #these are extra variables for printing formatting
         self.authorFull = author
         self.fullLine = title + pubYear + author
         
