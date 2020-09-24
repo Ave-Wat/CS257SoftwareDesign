34a60cf401ac7b5e6e9c62a3cafa122ca3371688
Ave-Wat <wattsa2@carleton.edu>
2020-09-23 20:30:34 -0500

    update book.py

--- books/book.py
+++ books/book.py
@@ -1,3 +1,19 @@
 class Book:
-    def __init__():
+    def __init__(title, pubYear, author):
+        self.title = title
+        self.pubYear = pubYear
+        authorList = author.split("(")
+        self.authorName = authorList[0].rstrip()
+        self.authorYears = authorList[1].rstrip()
+        
+    def getTitle():
+        return self.title
     
+    def getPubYear():
+        return self.pubYear
+    
+    def getAuthorName():
+        return self.authorName
+    
+    def getAuthorYears():
+        return self.authorYears
