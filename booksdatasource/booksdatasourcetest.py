a608055ef0bac8f1e70deed9dfb99910fef7c6ee
Ave-Wat <wattsa2@carleton.edu>
2020-09-29 11:34:36 -0500

    folder setup

---
+++ booksdatasource/booksdatasourcetest.py
@@ -0,0 +1,12 @@
+import BooksDataSource
+import unittest
+
+class BooksDataSourceTest(unittest.TestCase):
+    def setUp():
+
+    def tearDown():
+
+    def test_thing():
+
+if __name__ == '__main__':
+    unittest.main()
