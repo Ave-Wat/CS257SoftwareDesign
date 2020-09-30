52e85072223d56991a4dc94055bb36e7aa7f7eda
Ave-Wat <wattsa2@carleton.edu>
2020-09-30 10:05:30 -0500

    test outline

--- booksdatasource/booksdatasourcetest.py
+++ booksdatasource/booksdatasourcetest.py
@@ -3,10 +3,39 @@ import unittest
 
 class BooksDataSourceTest(unittest.TestCase):
     def setUp():
+        pass
 
     def tearDown():
+        pass
 
-    def test_thing():
+    #author tests
+    def test_author_name():
+        pass
+
+    def test_sort_birthyear():
+        pass
+
+    #books tests
+    def test_title_sort():
+        pass
+
+    def test_year_sort():
+        pass
+
+    def test_author_id():
+        pass
+
+    def test_search_text():
+        pass
+
+    def test_start_year():
+        pass
+
+    def test_end_year():
+        pass
+
+    def test_start_and_end_years():
+        pass
 
 if __name__ == '__main__':
     unittest.main()
