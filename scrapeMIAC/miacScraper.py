29fc56e464315c1b41e9ac0a35b7f7cc9b133275
Ave-Wat <wattsa2@carleton.edu>
2020-10-25 04:13:22 -0500

    added more years of scraping

--- scrapeMIAC/miacScraper.py
+++ scrapeMIAC/miacScraper.py
@@ -8,13 +8,16 @@ pip install requests or sudo pip3 install requests
 from bs4 import BeautifulSoup
 import requests
 import csv
+import tabula
+
+headers = requests.utils.default_headers()
+headers.update({
+'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+})
 
 def scrape2018Champ(url):
     athleteOutputList = []
-    headers = requests.utils.default_headers()
-    headers.update({
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
-    })
+
     response = requests.get(url, headers = headers)
     content = BeautifulSoup(response.content, "html.parser")
 
@@ -33,15 +36,14 @@ def scrape2018Champ(url):
 
                 athleteDict = {'event':event, 'name':name, 'team':team, 'time':time, 'place':place, 'year':year, 'location':location}
                 athleteOutputList.append(athleteDict)
+
+    writeToCSV("2014MIAC.csv", athleteOutputList)
+
     return athleteOutputList
 
 def scrape2017Champ(url):
-    print("run")
     athleteOutputList = []
-    headers = requests.utils.default_headers()
-    headers.update({
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
-    })
+
     response = requests.get(url, headers = headers)
     content = BeautifulSoup(response.content, "html.parser")
 
@@ -60,13 +62,82 @@ def scrape2017Champ(url):
 
                 athleteDict = {'event':event, 'name':name, 'team':team, 'time':time, 'place':place, 'year':year, 'location':location}
                 athleteOutputList.append(athleteDict)
+
+    writeToCSV("2014MIAC.csv", athleteOutputList)
+
     return athleteOutputList
+
+def scrape2016Champ():
+    # needs to be reordered to correct format in csv
+    file = "2016_MIAC_MXC_Results.pdf"
+    tabula.convert_into(file, "2016MIAC.csv", pages = "all")
+
+def scrape2015Champ():
+    #isn't working for SHIT
+    file = "2015_MIAC_MXC_Results.pdf"
+    tabula.convert_into(file, "2015MIAC.csv", pages = [2,3,4,5,6,7,8,9,10])
+
+def scrape2014Champ():
+    # need to amend data in csv
+    url = "https://www.miacathletics.com/playoffs/2014-15/XC_2014/miacmencc2014.html"
+    athleteOutputList = []
+    
+    response = requests.get(url, headers = headers)
+    content = BeautifulSoup(response.content, "html.parser")
+
+    paragraphList = content.find_all('p')
+
+    table = paragraphList[8].getText()
+    lines = table.split('\n')
+    lines = lines[1:-2] # had to get rid of blank lines
+
+    j = 0
+    for line in lines:
+        if j != 156 and j != 157:
+            lineList = line.split("  ")
+            '''place, place, name-grade, time, school'''
+            while '' in lineList:
+                lineList.remove('')
+
+            for i in range(0, len(lineList)):
+                lineList[i] = lineList[i].strip()
+            if lineList[1].isnumeric():
+                lineList.pop(1)
+
+            nameGradeList = lineList[1].split(",")
+
+            event = 'MIAC Championships'
+            name = nameGradeList[0]
+            team = lineList[3]
+            time = lineList[2]
+            place = lineList[0]
+            year = '2014'
+            location = 'Como Golf Course, St. Paul'
+
+            athleteDict = {'event':event, 'name':name, 'team':team, 'time':time, 'place':place, 'year':year, 'location':location}
+            athleteOutputList.append(athleteDict)
+        if j == 156: # the website is messed up on this particular line
+            list = ['Ernest Polania-Gonzalez','So','29:11.6','Carleton']
+            athleteDict = {'event':'MIAC Championships', 'name':list[0], 'team':list[3], 'time':list[2], 'place':'157', 'year':'2014', 'location':'Como Golf Course, St. Paul'}
+            athleteOutputList.append(athleteDict)
+        j = j + 1
+
+    writeToCSV("2014MIAC.csv", athleteOutputList)
+
+def writeToCSV(filename, athleteOutputList):
+    keys = athleteOutputList[0].keys()
+    with open(filename, "w", newline="") as file:
+        writer = csv.DictWriter(file, keys)
+        writer.writerows(athleteOutputList)
+        file.close()
 
 def main():
     fastFinishList = ['https://www.miacathletics.com/playoffs/2018-19/xc18/Men8kResults.htm', 'https://www.miacathletics.com/sports/mxc/2017-18/files/MIACMen.html']
     athleteList = []
-    athleteList = athleteList + (scrape2018Champ("https://www.miacathletics.com/playoffs/2018-19/xc18/Men8kResults.htm"))
-    athleteList = athleteList + (scrape2017Champ("https://www.miacathletics.com/sports/mxc/2017-18/files/MIACMen.html"))
-    print(athleteList)
+    scrape2018Champ("https://www.miacathletics.com/playoffs/2018-19/xc18/Men8kResults.htm")
+    scrape2017Champ("https://www.miacathletics.com/sports/mxc/2017-18/files/MIACMen.html")
+    scrape2016Champ()
+    scrape2015Champ()
+    scrape2014Champ()
 
 main()
