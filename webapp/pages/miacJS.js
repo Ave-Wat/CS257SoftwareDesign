b79a608533bef087e1e6a33f0aa9b3be57e4cd91
Ave-Wat <wattsa2@carleton.edu>
2020-11-06 11:01:29 -0600

    added search.html and updated javascript

--- webapp/pages/miacJS.js
+++ webapp/pages/miacJS.js
@@ -3,7 +3,6 @@
   3 November 2020
  */
 function screenDimensions() {
-  alert('hit dim')
   var win = window;
   var doc = document;
   var docElem = doc.documentElement;
@@ -13,24 +12,43 @@ function screenDimensions() {
   alert(x + ' × ' + y);
 }
 
+function collabpsibles(){
+  alert('collapsible')
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+}
+
+function directSearch(){
+  alert('search')
+  location.href = "search.html";
+}
+
 function initialize() {
-  //alert('hit init');
-  screenDimensions;
+  screenDimensions();
+
+  //doesn't work
+  var teamPerformanceButton = document.getElementById("team-performance");
+  var teamDepthButton = document.getElementById("team-depth");
+  var athleteDevButton = document.getElmentById("athlete-dev");
+  teamPerformanceButton.onclick = collapsibles;
+  teamDepthButton.onclick = collapsibles;
+  athleteDevButton.onclick = collapsibles;
+
+  //doesn't work
+  var searchButton = document.getElementbyId("input-search");
+  searchButton.onclick = directSearch;
 }
 
 window.onload = initialize;
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
