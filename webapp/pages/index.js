7a14df3c6fe6bb3cbaf8c1bb7885d033b6c4cc68
Ave-Wat <wattsa2@carleton.edu>
2020-11-08 11:53:29 -0600

    fixed collapsibles

--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -17,7 +17,6 @@ function collapsibles(){
   var i;
 
   for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
       if (content.style.display === "block") {
@@ -25,7 +24,6 @@ function collapsibles(){
       } else {
         content.style.display = "block";
       }
-    });
   }
 }
 
