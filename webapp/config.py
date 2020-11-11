37a1c19af31bbc2d4beb33debc83354b5f20be79
aokib <aokib@carleton.edu>
2020-11-10 22:36:40 -0600

    rough draft of js for search

--- webapp/config.py
+++ webapp/config.py
@@ -1,4 +1,4 @@
 # Change these values as appropriate for your postgresql setup.
-database = ''
-user = ''
+database = 'webapp'
+user = 'aokib'
 password = ''
--- webapp/static/index.js
+++ webapp/static/index.js
@@ -18,29 +18,88 @@ function collapsibles(){
 }
 
 function onSearchButton(){
-  location.href = "/search.html"
-  /*var inputElement = document.getElementById('champ-search');
-  var inputValue = inputElement.value;
-
-  var radioButtons = document.getElementsByName('search-form');
-  var radioValue;
+  location.href = "/search.html";
+  var searchBar = document.getElementById('champ-search');
+  var keyword = searchBar.value;
+  var radioButtons = document.getElementsByName('search-field');
   for(i = 0; i < radioButtons.length; i++) {
     if(radioButtons[i].checked)
-      document.getElementById("result").innerHTML
-      radioValue = ele[i].value;
+      searchField = radioButtons[i].value;
     }
   }
-  // /search?field=[athletes,teams,year]&keyword={search_text}
-  var url = getAPIBaseURL() + '/search?field=[' + radioValue + ']&keyword={' + inputValue + '}';
+
+  var resultsHeaderElement = document.getElementById('results-header');
+  resultsHeaderElement.innerHTML = 'Results for' + keyword + 'in' + searchField;
+
+  var url = getAPIBaseURL() + '/search?field=[' + searchField + ']&keyword={' + keyword + '}';
   fetch(url, method: 'get')
 
   .then((response) => response.json())
-  .then(function(resultList) {
 
-  })*/
+  if searchField == 'athletes' {
+    .then(displayAthletesResults(athletesList))
+  } else if searchField == 'teams' {
+    .then(displayTeamsResults(teamsList))
+  } else {
+    .then(displayYearResults(yearResults))
+  }
+  .catch(function(error) {
+      console.log(error);
+  });
 
 }
-
+//athletesList: [name, team, place, time, year]
+function displayAthletesResults(athletesList) {
+  var divBody = '<table>';
+  for (var i=0; i < athletesList.length; i++) {
+    divBody += '<tr>';
+    for (var key in athletesList[i]) {
+      divBody += '<td>' + athletesList[i][key] + '</td>';
+    }
+    divBody += '</tr>'';
+  }
+  divBody += '</table>';
+  var resultsDivElement = document.getElementById('search-results');
+  resultsDivElement.innerHTML = divBody;
+}
+//teamsList: [name, location, place, points, year]
+function displayTeamsResults(teamsList) {
+  var divBody = '<table>';
+  for (var i=0; i < teamsList.length; i++) {
+    divBody += '<tr>';
+    for (var key in teamsList[i]) {
+      divBody += '<td>' + teamsList[i][key] + '</td>';
+    }
+    divBody += '</tr>';
+  }
+  divBody += '</table>';
+  var resultsDivElement = document.getElementById('search-results');
+  resultsDivElement.innerHTML = divBody;
+}
+//two lists: [[name, place, points, location],[name, team, place, time]]
+function displayYearResults(yearResults) {
+  var divBody = '<p>Team Results: </p><table>';
+  var teamResults = yearResults[0];
+  var individualResults = yearResults[1];
+  for (var i=0; i < teamResults; i++) {
+    divBody += '<tr>';
+    for (var key in teamResults[i]) {
+      divBody += '<td>' + teamResults[i][key]; + '</td>';
+    }
+    divBody += '</tr>';
+  }
+  divBody += '</table><p>Individual Results: </p><table>'
+  for (var j=0; j < individualResults; j++) {
+    divBody += '<tr>';
+    for (var key in individualResults[i]) {
+      divBody += '<td>' + individualResults[i][key] + '</td>';
+    }
+    divBody += '</tr>';
+  }
+  divBody += '</table>';
+  var resultsDivElement = document.getElementById('search-results');
+  resultsDivElement.innerHTML = divBody;
+}
 // Returns the base URL of the API, onto which endpoint components can be appended.
 function getAPIBaseURL() {
     var baseURL = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/api';
